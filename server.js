784a924c0034a66cbb78224db842aac41a8c058e
Elijah McKay <elijahmckay10@gmail.com>
2019-10-23 14:51:29 -0500

    mongoose boiler plate and environment variables set up.  Pushing to master and then switching to `models` branch

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const router = require('express').Router()
 
 const server = express(); 
 
+// library imports 
+const dotenv = require('dotenv'); 
+
+// setting up mongoose 
+mongoose.connect(
+	`mongodb+srv://projectfirefly:${process.env.PASSWORD}@project-firefly-qsjuq.mongodb.net/test?retryWrites=true&w=majority`)
+	{
+		useMongoClient: true
+	}
 // middleware instantiation
 server.use(express.json()); 
 server.use(helmet()); 
