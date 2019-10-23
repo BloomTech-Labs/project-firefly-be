1c2d134a224f238cd972e6fdda6002d579f6eb00
Elijah McKay <elijahmckay10@gmail.com>
2019-10-23 12:11:11 -0500

    reset master branch, pushing so everyone can merge

--- data/dbConfig.js
+++
@@ -1,7 +0,0 @@
-const environment = process.env.DB_ENV || 'development';
-
-const knex = require('knex');
-
-const knexConfig = require('../knexfile.js')[environment];
-
-module.exports = knex(knexConfig);
--- resolvers/index.js
+++
@@ -1,4 +0,0 @@
-const GMR = require("graphql-merge-resolvers");
-const userResolvers = require("./users/userResolvers.js");
-
-module.exports = GMR.merge([userResolvers]);
