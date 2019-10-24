07ffd93f704aa542896e90a22c1f2a6600bed118
Elijah McKay <elijahmckay10@gmail.com>
2019-10-23 20:01:41 -0500

    fixed syntax bugs in children-routes.js

--- routes/children-routes.js
+++ routes/children-routes.js
@@ -7,45 +7,43 @@ router.get('/', (req, res) => {
     Children.find()
 	.then(child => res.status(200).json(child))
 	.catch(err => err.status(500).json(res))
-}
+})
 
 // get specific children 
 router.get('/:_id', (req, res) => {
     const { _id } = req.params; 
 
     Users.findById({ _id })
-	.then(child => res.status(200).json(user))
+	.then(child => res.status(200).json(child))
 	.catch(err => res.status(500).json(err))
-}
+})
 
 // add new child profile 	
 router.post('/', (req, res) => {
-    const child = new Children({
-	name: blahblah
-    })
+    const child = new Children({ })
 
     child.save()
 	.then(newChild => res.status(201).json(newUser))
 	.catch(err => res.status(500).json(err)); 
-}
+})
 
 // update existing child profile
-router.put'/:_id', (req, res) => {
+router.put('/:_id', (req, res) => {
     const { _id } = req.params; 
     const changes = req.body; 
 
     Children.findByIdAndUpdate(_id, changes)
 	.then(updatedChild => res.status(200).json(updatedChild))
-	.catch(err => res.status(500).json(err)
-}
+	.catch(err => res.status(500).json(err))
+})
 
 // delete existing child profile 
 router.delete('/:_id', (req, res) => {
     const { _id } = req.params; 
 
-    Children.findByIdandDelete(_id)
+    Children.findByIdAndDelete(_id)
 	.then(deletedChild => res.status(200).json(deletedChild))
 	.catch(err => res.status(500).json(err)); 
-}
+})
 
 module.exports = router; 
