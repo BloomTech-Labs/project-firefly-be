f5c8006a09cdd00e78cf58c36c33aa586c9b8244
Arvin Agas <arvagas@gmail.com>
2019-10-31 17:29:35 -0400

    created firebase utils to use for authentication

---
+++ utils/firebase.js
@@ -0,0 +1,12 @@
+// Import library
+const firebase = require("firebase-admin");
+require('dotenv/config');
+
+// Import account that will be used
+const serviceAccount = require("./firebase-service-account.json");
+
+// Export Firebase initilization
+module.exports = firebase.initializeApp({
+  credentials: firebase.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
